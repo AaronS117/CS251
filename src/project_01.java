6bf40e0a933423b8e01b2331b3ade48b80a38646
AaronS117 <aaronoropeza98@gmail.com>
2022-09-15 16:33:35 -0500

    almost done with methods

--- src/project_01.java
+++ src/project_01.java
@@ -2,6 +2,7 @@ public class project_01
 {
     public static void main(String[] args)
     {
+        // Mandatory code copy
         boolean[] myDeck = new boolean[52];
         final int cardsPerRow = 8;
         int cardsThisRow = 0;
@@ -26,28 +27,56 @@ public class project_01
             }
         }
         System.out .println('\n');
-    }
-    public static void initDeck(boolean[] deck) {
+    }// End Main
+
+
+    public static void initDeck(boolean[] deck)
+    {
+        // set the values of deck to indicate that they are all
+        // present - not dealt yet.
+
         for (int i = 0; i <= 51; i++)
             deck[i] = true;
     }
-    public static boolean emptyDeck(boolean[] deck){
-        for (int i = 0; i < 51; i++) {
-            if (deck[i]){
+
+    public static boolean emptyDeck(boolean[] deck)
+    {
+        // returns whether all the cards in the deck
+        // have already been dealt.
+
+        for (int i = 0; i < 51; i++)
+        {
+            if (deck[i] == true){
                 return false;
             }
         }
         return true;
     }
-    public static int dealCard(boolean[] deck) {
-        while (!emptyDeck(deck)){
-            for (int j = 0; j < 51; j++) {
+
+    public static int dealCard(boolean[] deck)
+    {
+        // returns a card (an int in the range 0 to 51) at random
+        // that has not been dealt since the deck was initialize
+        // via in Deck. Also notes (in deck) that this card is
+        // no longer available.
+
+        int a = (int) (Math.random()*(51));
+        if (deck[a] == false){
+            while (deck[a] == false){
+                a = (int) (Math.random()*(51));
             }
         }
-
-        return 0;
+        deck [a] = false;
+        System.out.print(a);
+        return a;
     }
-    public static void printCard(int card){
+    public static void printCard(int card)
+    {
+        // given a card (an int in the range 0 to 51) prints
+        // an appropriate representation of this card based
+        // on a 1-1 and onto mapping of the set [0, 51] to
+        // the cards described above.
+
         card = 1;
     }
 }
